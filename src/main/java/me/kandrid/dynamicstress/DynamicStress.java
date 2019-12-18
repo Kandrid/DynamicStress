f02eb3fb32528727cefc102471e027381a9ab853
Nick <18233355+Kandrid@users.noreply.github.com>
2019-12-18 19:07:55 -0400

    Organize

--- src/main/java/me/kandrid/dynamicstress/DynamicStress.java
+++ src/main/java/me/kandrid/dynamicstress/DynamicStress.java
@@ -63,7 +63,7 @@ public final class DynamicStress extends JavaPlugin {
         final double precision = 1;
         final Vector eVector = new Vector((eLocation.getX() - pLocation.getX()) / eDistance, (eLocation.getY() - pLocation.getY()) / eDistance, (eLocation.getZ() - pLocation.getZ()) / eDistance);
         final Vector pVector = pLocation.getDirection();
-        final double angle = Math.toDegrees(pVector.angle(eVector));//Math.abs(Math.toDegrees(Math.acos((eVector.getX() * pVector.getX() + eVector.getZ() * pVector.getZ()) / (eVector.length() * pVector.length()))));
+        final double angle = Math.toDegrees(pVector.angle(eVector));
 
         if (angle <= fov) {
             for (double i = 0; i < maxDistance; i += precision) {
@@ -71,9 +71,11 @@ public final class DynamicStress extends JavaPlugin {
                     player.sendRawMessage("Type:" + entity.getName() + " ID:" + entity.getEntityId() + " Dist:" + Math.round(eDistance * 10.0) / 10.0 + " Angle:" + Math.round(angle));
                     return true;
                 }
+
                 Vector step = pLocation.toVector().add(eVector.clone().multiply(i));
                 Material m1 = player.getWorld().getBlockAt((int)Math.floor(step.getX()), (int)Math.floor(step.getY()), (int)Math.floor(step.getZ())).getType();
                 Material m2 = player.getWorld().getBlockAt((int)Math.floor(step.getX()), (int)Math.floor(step.getY() - 1), (int)Math.floor(step.getZ())).getType();
+
                 if (m1 != Material.LARGE_FERN && m1 != Material.FERN && m1 != Material.AIR && m1 != Material.CAVE_AIR && m1 != Material.VOID_AIR && m1 != Material.TALL_GRASS && m1 != Material.GRASS && m1 != Material.TALL_SEAGRASS && m1 != Material.WATER && m1 != Material.GLASS && m1 != Material.GLASS_PANE && m1 != Material.VINE && m2 != Material.AIR && m2 != Material.CAVE_AIR && m2 != Material.VOID_AIR && m2 != Material.TALL_GRASS && m2 != Material.GRASS && m2 != Material.TALL_SEAGRASS && m2 != Material.WATER && m2 != Material.GLASS && m2 != Material.GLASS_PANE &&  m2 != Material.VINE && m2 != Material.LARGE_FERN && m2 != Material.FERN) {
                     break;
                 }
