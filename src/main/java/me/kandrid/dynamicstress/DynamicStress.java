15600a9ec39705d260dd2c57b96b4c38bb775b63
Nick <18233355+Kandrid@users.noreply.github.com>
2019-12-18 20:52:10 -0400

    Remove import

--- src/main/java/me/kandrid/dynamicstress/DynamicStress.java
+++ src/main/java/me/kandrid/dynamicstress/DynamicStress.java
@@ -13,7 +13,6 @@ import org.bukkit.util.Vector;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
 public final class DynamicStress extends JavaPlugin {
 
